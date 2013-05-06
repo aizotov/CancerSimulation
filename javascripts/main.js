9d8f1550d18f61ff25e393b67d2dd949512a5d33
Andrey Izotov <izotov100@gmail.com>
2013-05-06 03:25:54 -0400

    fixed website

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,5 +2,20 @@
 	$(function(){
   		// carousel demo
   		$('#myCarousel').carousel()
+
+		$("#documentationDownloadBtn").click(function( event ) {
+	      	window.location.href = 'docs/CancerSimulationDescription.pdf';
+	        event.preventDefault();
+	   	});
+
+		$("#usageInstructionsDownloadBtn").click(function( event ) {
+	      	window.location.href = 'docs/Instructions.pdf';
+	        event.preventDefault();
+	   	});
+
+	   	$("#exampleCancersDownloadBtn").click(function( event ) {
+	      	window.location.href = 'docs/ExampleCancers.pdf';
+	        event.preventDefault();
+	   	});
 	})
 }(window.jQuery)
